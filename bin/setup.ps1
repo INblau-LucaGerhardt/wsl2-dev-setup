d5272af2090ce36650d5642bfee89a8cfa0d1d73
INblau-LucaGerhardt <95220064+INblau-LucaGerhardt@users.noreply.github.com>
2022-01-24 11:35:19 +0100

    Create setup.ps1

---
+++ bin/setup.ps1
@@ -0,0 +1,27 @@
+# install wsl2
+wsl --install
+wsl --set-default-version 2
+
+# install Ubuntu 20.04 LTS
+winget install -i --id Canonical.Ubuntu.2004
+
+# install PHPStorm
+winget install -i --id JetBrains.PHPStorm
+
+# install Postman
+winget install -i --id Postman.Postman
+
+# install Docker
+winget install -i --id Docker.DockerDesktop
+
+# install 1Password
+winget install -i --id AgileBits.1Password
+
+# install HeidiSQL
+winget install -i --id HeidiSQL.HeidiSQL
+
+# install Remote Desktop
+winget install -i --id Microsoft.RemoteDesktopClient
+
+# install 7zip
+winget install -i --id 7zip.7zip
